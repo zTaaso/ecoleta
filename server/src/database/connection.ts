1d20bd325ce4d9769073eefc0d373fe0b29efe75
zTaaso <ztaaso.cedaspy@gmail.com>
2020-06-03 03:37:48 -0300

    database edited

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,1 +1,12 @@
-import {} from 'path';
+import { resolve } from 'path';
+import knex from 'knex';
+
+const connection = knex({
+	client: 'sqlite3',
+	connection: {
+		filename: resolve(__dirname, 'database.sqlite'),
+	},
+	useNullAsDefault: true,
+});
+
+export default connection;
