30115f8b9b88ec72a79d621ef9d30e206db477a2
zTaaso <ztaaso.cedaspy@gmail.com>
2020-06-04 17:45:10 -0300

    cors added

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 class App {
 	server: express.Express;
@@ -12,7 +13,7 @@ class App {
 	}
 
 	middlewares() {
-		// this.server.use(cors())
+		this.server.use(cors());
 		this.server.use(express.json());
 	}
 
